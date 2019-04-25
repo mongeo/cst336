51abe289452c1da3cfe66fadbcb212ed12733502
Geoffrey Montague <gmontague@csumb.edu>
2019-04-25 23:39:55 +0000

    remainder

--- labs/lab6/api/getProducts.php
+++ labs/lab6/api/getProducts.php
@@ -1,18 +1,10 @@
 <?php
 
-$host = "localhost";
-$dbname = "ottermart";
-$username = "root";
-$password = "";
-
-// Establishing a connection
-$dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-// Setting Errorhandling to Exception
-$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+include '../../../inc/dbConnection.php';
+$conn = getDatabaseConnection("ottermart");
 
 $sql = "SELECT * FROM om_product ORDER BY productPrice LIMIT 10";
-$stmt = $dbConn -> prepare($sql);  //$connection MUST be previously initialized
+$stmt = $conn -> prepare($sql);  //$connection MUST be previously initialized
 $stmt->execute();
 $records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
 
